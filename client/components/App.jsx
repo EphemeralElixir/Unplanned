981f20cd5e41a50878861193f89a5ccfb40a582e
Leo Adelstein <leoadelstein@gmail.com>
2016-05-26 15:59:47 -0700

    react boilerplate with webpack & babel

---
+++ client/components/App.jsx
@@ -0,0 +1,12 @@
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+class App extends React.Component {
+  render() {
+    return <div>Hello World</div>;
+  }
+}
+
+
+window.App = App;
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var debug = process.env.NODE_ENV !== "production";
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  context: path.join(__dirname, ""),
+  devtool: debug ? "inline-sourcemap" : null,
+  entry: "./client/webpackEntry.js",
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015'],
+        }
+      }
+    ]
+  },
+  output: {
+    path: __dirname + "/client/public",
+    filename: "webpack.min.js"
+  },
+  plugins: debug ? [] : [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+  ],
+};
