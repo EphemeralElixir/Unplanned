92a1c62eb92d8b695f57a0317f25f2b968ffd27b
Anhtai Huynh <anhtaih89@gmail.com>
2016-05-26 11:53:15 -0700

    Set up initial user schema

---
+++ server/config/env/auth.config.js
@@ -0,0 +1,10 @@
+//Fill in and change name to Auth.js in production
+
+module.exports = {
+  'facebookAuth': {
+    callbackURL: 'InsertCallbackURL',
+    enableProof: true,
+    clientID: 'InsertClientID',
+    clientSecret: 'InsertClientSecret'
+  }
+};
