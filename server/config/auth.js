a9cbe5e2c0c8723b5db13468ad709f34edb0c0a0
Tai Huynh <anhtaih89@gmail.com>
2016-05-27 19:45:40 -0700

    Remove main auth file

--- server/config/auth.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  'facebookAuth': {
-    clientID: '547544362084907', //need to export this data to env file later
-    clientSecret: '74e7d3258d599007d47ec0e6bdbe31e3', //need to export it to env file later
-    callbackURL: 'http://localhost:8000/auth/facebook/callback'
-  }
-};
