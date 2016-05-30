28367e0ef5954c932ac97e0b1cb9e56e351c6c06
heysep <sdvakili@gmail.com>
2016-05-30 09:57:30 -0700

    Add Gmap component instance to App component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,33 +1,19 @@
-<<<<<<< 758e49cda3bc49247a8cfd8c88c0f84b5999b46e
 import React, { Component } from 'react'
 import AddUser from './AddUser.jsx';
 import UserList from './UserList.jsx';
-
-class App extends React.Component {
-  render() {
-    return (<div>
-    	<p>Hello World</p>
-      <AddUser/>
-      <UserList/>
-    </div>);
-=======
-import { default as React } from 'react';
 import { default as Gmap } from './Gmap.jsx';
-
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <div>Hello World</div>
-        <div>
-          <Gmap />
-        </div>
+    	 <p>Hello World</p>
+        <AddUser/>
+        <UserList/>
+        <Gmap />
       </div>
-    )
->>>>>>> (refactor) use es6 import instead of require
+    );
   }
 }
-
 
 export default App;
