7e8f82a124c3ba780d4a1f8bcf3921731978c460
Shane Hubbell <shane.hubbell@gmail.com>
2016-05-31 11:23:56 -0700

    Working through bugs on the timer

---
+++ client/components/CountdownTimer.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+class CountdownTimer extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+          secondsRemaining: 0
+      };
+  }
+
+  tick () {
+    if (this.state.secondsRemaining <= 1) {
+      clearInterval(this.interval);
+      
+    }
+    return this.setState({secondsRemaining: this.state.secondsRemaining - 1});
+  }
+
+  componentDidMount () {
+    this.setState({secondsRemaining: 30});
+    this.interval = setInterval(this.tick.bind(this), 1000);
+  }
+  
+  componentWillUnmount () {
+    clearInterval(this.interval);
+  }
+
+  render () {
+    return (<div>Seconds Remaining: {this.state.secondsRemaining}</div>)
+  }
+}
+
+export default CountdownTimer;
