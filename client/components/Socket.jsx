d11a89cc047f1f55e2b8b4f2cd0299ad4aa49e38
Leo Adelstein <leoadelstein@gmail.com>
2016-05-31 17:26:20 -0700

    integrated socket with redux, fixed bugs

--- client/components/Socket.jsx
+++ client/components/Socket.jsx
@@ -1,20 +1,23 @@
 import React from 'react'
+import actions from '../redux/actions.js'
 
 class Socket extends React.Component {
 
 	constructor(props) {
 		super(props);
 		this.socket = io.connect('http://localhost:8000');
-		
+
 		this.socketUsers = {};
 
 		this.socketUser = {
 			userID: '',
 			picture: '',
 			name: '',
-			bio: ''
+			bio: '',
+			lat: '',
+			lng: ''
 		};
-
+  
 
 		window.FB.login(function(response) {
 			var that = this;
@@ -35,12 +38,34 @@ class Socket extends React.Component {
 	}
 
 	componentDidMount() {
-		setTimeout(sendToServer.bind(this), 500);
+		setInterval(this.sendToServer.bind(this), 3000);
+		setInterval(this.updateLocation.bind(this), 3000);
+		this.socket.on('update all users', this.updateUserList.bind(this));
 	}
 
 	sendToServer() {
-		this.socket.
+		console.log('callingn send to server');
+		if (this.socket.connected) {
+			this.socket.emit('update one socket user', this.socketUser, this.socket.id);
+		}
 	}
+
+	updateLocation() {
+		var that = this;
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(function(position) {
+				console.log('%%%%%%%%%%%%%%%%%%%', that.socketUser)
+				that.socketUser.lat = position.coords.latitude;
+				that.socketUser.lng = position.coords.longitude;
+			});
+		}
+	};
+
+	updateUserList(activeUsers) {
+		console.log('all users from socket server', activeUsers);
+		this.props.dispatch(actions.updateUserList(activeUsers));
+	}
+
 
 
 
