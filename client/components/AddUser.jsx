20ef0427f5ca45c30aa906833b43c8cbdd17a990
heysep <sdvakili@gmail.com>
2016-05-31 20:14:54 -0700

    Refactor to meet airbnb styleguide

--- client/components/AddUser.jsx
+++ client/components/AddUser.jsx
@@ -1,39 +1,48 @@
 import React, { Component } from 'react';
-import actions from '../redux/actions.js'
+import actions from '../redux/actions.js';
 
 class AddUser extends Component {
 
-	constructor(props) {
-		super(props);
-		this.state = {
-			inputName: ''
-		}
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputName: '',
+    };
+    // use the following binding pattern per airbnb style guide:
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-no-bind.md
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-	handleChange(event) {
-		this.setState({
-			inputName: event.target.value
-		});
-	}
+  handleChange(event) {
+    this.setState({
+      inputName: event.target.value,
+    });
+  }
 
-	handleSubmit(event) {
-		event.preventDefault();
-		console.log('submit button clicked');
-		this.props.dispatch(actions.addUser(66, this.state.inputName));
-	}
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log('submit button clicked');
+    this.props.dispatch(actions.addUser(66, this.state.inputName));
+  }
 
-	render() {
-		return (<div>
-		  <input
-				  type="text"
-				  placeholder="new user's name"
-				  value={this.state.inputName}
-				  onChange={this.handleChange.bind(this)}
-				/>
-			<button onClick={this.handleSubmit.bind(this)}> Submit </button>
-
-		</div>);
-	}
+  render() {
+    return (
+      <div>
+        <input
+          type="text"
+          placeholder="new user's name"
+          value={this.state.inputName}
+          onChange={this.handleChange}
+        />
+        <button onClick={this.handleSubmit}> Submit </button>
+      </div>
+    );
+  }
 }
+
+AddUser.propTypes = {
+  dispatch: React.PropTypes.func,
+};
 
 export default AddUser;
--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,30 +1,32 @@
-import React, { Component } from 'react'
+import React from 'react';
 import AddUser from './AddUser.jsx';
 import UserList from './UserList.jsx';
 import Options from './Options.jsx';
 import MeetingRequest from './MeetingRequest.jsx';
 import { default as Gmap } from './Gmap.jsx';
 import { connect } from 'react-redux';
-import Socket from './Socket.jsx'
+import Socket from './Socket.jsx';
 
-class App extends React.Component {
-  render() {
-    return (<div>
-    	<p>Hello World</p>
-      <AddUser dispatch={this.props.dispatch}/>
-      <UserList userList={this.props.users}/>
-      <Gmap users={this.props.users}/>
-      <Options/>
-      <MeetingRequest/>
-
-      <Socket dispatch={this.props.dispatch}/>
-    </div>);
-  }
-}
+const App = (props) => (
+  <div>
+    <p>Hello World</p>
+    <AddUser dispatch={props.dispatch} />
+    <UserList userList={props.users} />
+    <Gmap users={props.users} />
+    <Options />
+    <MeetingRequest />
+    <Socket dispatch={props.dispatch} />
+  </div>
+);
 
 // only reaturn the part of the state/store that the component needs
 function mapStateToProps(state) {
-	return state;
+  return state;
 }
+
+App.propTypes = {
+  dispatch: React.PropTypes.func,
+  users: React.PropTypes.object,
+};
 
 export default connect(mapStateToProps)(App);
