8295186dd4002d51b86e8a5506283cea3403e17c
Leo Adelstein <leoadelstein@gmail.com>
2016-06-01 18:37:58 -0700

    integrating socket and redux & testing

--- client/components/Socket.jsx
+++ client/components/Socket.jsx
@@ -63,6 +63,7 @@ class Socket extends React.Component {
 
     window.socket.api.sendMeetingRequest = function sendMeetingRequest(receiverId) {
       if (window.socket.connected && window.isLoggedIn) {
+        console.log(window.socket.id, receiverId, '!!!!!!!!!!!!!!!');
         window.socket.emit('send meeting request', window.socket.id, receiverId);
       }
     };
@@ -85,10 +86,29 @@ class Socket extends React.Component {
     setInterval(window.socket.api.updateLocation, 5000);
 
     window.socket.on('update all users', this.updateUserList.bind(this));
+
+    window.socket.on('receive meeting request', this.receivedMeetingRequest.bind(this));
+    window.socket.on('confirm meeting request', this.recievedConfirmation.bind(this));
+    window.socket.on('reject meeting request', this.receivedRejection.bind(this));
   }
 
   updateUserList(activeUsers) {
     this.props.dispatch(actions.updateUserList(activeUsers));
+  }
+
+  receivedMeetingRequest(requesterId) {
+    alert('this guy wants to meet me', requesterId);
+    this.props.dispatch(actions.setRequester(requesterId));
+  }
+
+  recievedConfirmation(acceptedId) {
+    alert('this guys confirmed', acceptedId);
+    this.props.dispatch(actions.setAccepted(acceptedId));
+  }
+
+  receivedRejection() {
+    alert('i got rejected');
+    this.props.dispatch(actions.clearMeet());
   }
 
   render() {
