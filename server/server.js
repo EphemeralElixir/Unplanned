b6df788e8c7f8ffb8438c94c7da39998801a4936
Tai Huynh <anhtaih89@gmail.com>
2016-06-03 09:45:49 -0700

    Refactor user controller to use shorthand function, remove hardcoding of port to allow deployment to configure

--- server/server.js
+++ server/server.js
@@ -1,19 +1,20 @@
 const express = require('express');
 const app = express();
-
 const http = require('http').Server(app);
-
 const mongoose = require('mongoose');
+const io = require('./config/socket');
+
+app.set('port', process.env.PORT || 8000);
+
+io.makeSocketServer(http);
 
 mongoose.connect('mongodb://localhost/elixir');
 
-const io = require('./config/socket');
-io.makeSocketServer(http);
 
 require('./config/middleware.js')(app, express);
 
-http.listen(8000, () => {
-  console.log('Server is setup. Listening on port 8000...');
+http.listen(app.get('port'), () => {
+  console.log('Express server started in ' + app.get('env') + ' mode on port ' + app.get('port'));
 });
 
 module.exports = app;
