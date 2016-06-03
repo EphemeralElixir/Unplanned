74abd42f27bd70dc12d29003d4d5a60898e3eea4
heysep <sdvakili@gmail.com>
2016-06-03 12:21:17 -0700

    Ensure nav bar provides login option if user is not logged in

--- client/components/Splash.jsx
+++ client/components/Splash.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Splash = function Splash() {
-  return (<div>
+  return (<div className="splash">
     <h2> Let's build out the Splash Component </h2>
   </div>);
 
