f6a8ab37b3724947f212c2723cfb7ab88d01eda9
heysep <sdvakili@gmail.com>
2016-06-03 12:08:52 -0700

    Add styling to Nav and Options components

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -7,9 +7,21 @@ const Nav = function Nav() {
     </div>);
   }
   return (
-    <div className="pure-menu pure-menu-horizontal">
-      <button type="button" onClick={window.socket.api.login}>Login with facebook </button>
-    </div>);
+    <div className="header">
+      <div className="pure-menu pure-menu-fixed pure-menu-horizontal home-menu">
+        <a className="pure-menu-heading" href="/">unplan!</a>
+        <span>Welcome {window.socket.api.user.name}</span>
+        <ul className="pure-menu-list">
+          <li className="pure-menu-item">
+            <a href="#">My Profile</a>
+          </li>
+          <li className="pure-menu-item">
+            <a href="#">Logout</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Nav;
