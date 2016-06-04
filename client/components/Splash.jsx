6b37645ff2ea8bc19c5475001e309df1a0054484
Tai Huynh <anhtaih89@gmail.com>
2016-06-04 13:12:49 -0700

    Style splash and add content to splash page

--- client/components/Splash.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Splash = function Splash() {
-  return (<div className="splash">
-    <h2> Let's build out the Splash Component </h2>
-  </div>);
-};
-
-export default Splash;
---
+++ client/components/splash/Adventure.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+
+const Adventure = function Adventure() {
+  return (
+    <div className="content">
+      <h2 className="content-head is-center">Go on an adventure RIGHT NOW</h2>
+      <div className="pure-g">
+        <div className="l-box pure-u-1 pure-u-md-1-2 pure-u-lg-1-4">
+          <h3 className="content-subhead">
+            <i className="fa fa-rocket"></i>
+             Gut Instinct
+          </h3>
+          <p>
+            Unplanned is an app that will help you meet new people spontaneously.
+            It works by giving you only 13 seconds to make a decision on whether
+            or not you'd like to meet the person who wants to meet you.
+          </p>
+        </div>
+        <div className="l-box pure-u-1 pure-u-md-1-2 pure-u-lg-1-4">
+          <h3 className="content-subhead">
+            <i className="fa fa-mobile"></i>
+              Spontaneous
+          </h3>
+          <p>
+            How about a simple hangout over coffee, food, or beer? Make a decision
+            NOW, and then go do it. No thinking allowed.
+          </p>
+        </div>
+        <div className="l-box pure-u-1 pure-u-md-1-2 pure-u-lg-1-4">
+          <h3 className="content-subhead">
+            <i className="fa fa-th-large"></i>
+              Happy and Healthy
+          </h3>
+          <p>
+            Despite being ever-more connected, we still end up hanging out with the
+            same group of people on a daily basis, rarely meeting new people and
+            being spontaneous. Studies have shown that life full of quality
+            relationships is a happy,
+          </p>
+        </div>
+        <div className="l-box pure-u-1 pure-u-md-1-2 pure-u-lg-1-4">
+          <h3 className="content-subhead">
+            <i className="fa fa-check-square-o"></i>
+            Connect With People
+          </h3>
+          <p>
+            Signing up is simple. Log in with Facebook, make yourself available to
+            meet new people, and go have coffee, food, or beer with someone new.
+          </p>
+        </div>
+      </div>
+    </div>
+    );
+};
+
+
+export default Adventure;
